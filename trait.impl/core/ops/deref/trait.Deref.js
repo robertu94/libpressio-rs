9f58dd43fae013fe4c2ef90dbbbb8fd1d9aed3b0
robertu94 <robertu94@users.noreply.github.com>
2026-03-28 14:22:12 +0000

    deploy: f621382e7e76fc8e2060a476a70b764c2539e9f3

---
+++ trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["libpressio",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"libpressio/struct.PressioSendableCompressor.html\" title=\"struct libpressio::PressioSendableCompressor\">PressioSendableCompressor</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[338]}
---
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["libpressio",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"libpressio/struct.PressioSendableCompressor.html\" title=\"struct libpressio::PressioSendableCompressor\">PressioSendableCompressor</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[347]}
